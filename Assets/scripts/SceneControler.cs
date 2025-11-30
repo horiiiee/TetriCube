252536839a0d6188086520c5bc5859e116d44fb0
horiiiee <anastasiya.serkova.2005@gmail.com>
2025-11-30 23:29:43 +0300

    design and interface
    создала главное меню, оформила его, добавила туда кнопки: играть, настройки, выход из игры. в окне игры добавила кнопки вниз, направо и налево, кнопка паузы, в панеле паузы кнопка выход в главное меню, кнопка продолжить, и кнопка выключения звука.  сделала панель геймовер, там добавила счет, лучший счет, кнопка повторить, и кнопка в главное меню.

--- Assets/scripts/SceneControler.cs
+++ Assets/scripts/SceneControler.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneControler : MonoBehaviour
 {
+    [SerializeField] public GameObject PausePanel;
+    [SerializeField] public GameObject OptionsPanel;
+    [SerializeField] private AudioSource audioSource;
     private bool pause = false;
 
     private void Update()
@@ -17,11 +21,13 @@ public class SceneControler : MonoBehaviour
         {
             Time.timeScale = 0f;
             pause = true;
+            PausePanel.SetActive(true);
         }
         else
         {
             Time.timeScale = 1f;
             pause = false;
+            PausePanel.SetActive(false);
         }
     }
 
@@ -29,5 +35,50 @@ public class SceneControler : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("вышли из игры");
+    }
+    public static void LoadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ToggleSound()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void DecreaseVolume(float amount)
+    {
+        audioSource.volume -= amount;
+
+        if (audioSource.volume < 0f)
+        {
+            audioSource.volume = 0f;
+        }
+    }
+
+    public void AddVolume(float amount)
+    {
+        audioSource.volume += amount;
+
+        if (audioSource.volume > 1f)
+        {
+            audioSource.volume = 1f;
+        }
+    }
+
+    public void OpenOptions()
+    {
+        OptionsPanel.SetActive(true);   
+    }
+    public void CloseOptions()
+    {
+        OptionsPanel.SetActive(false);
     }
 }
